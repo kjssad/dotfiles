5d94bbc609083161bd9d74beba3afee2cfd6f6e4
kjssad <kimsadomia@protonmail.com>
2019-07-30 19:59:13 +0800

    Add Gnome DE keybindings

---
+++ gnome/keybindings.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['<Super><Ctrl>k']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['<Super><Ctrl>j']"
+
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-1 "['<Shift><Super>exclam']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-2 "['<Shift><Super>at']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-3 "['<Shift><Super>numbersign']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-4 "['<Shift><Super>dollar']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-5 "['<Shift><Super>percent']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-6 "['<Shift><Super>asciicircum']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-7 "['<Shift><Super>ampersand']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-8 "['<Shift><Super>asterisk']"
+
+gsettings set org.gnome.desktop.wm.keybindings switch-applications "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "[]"
+
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 "['<Super>5']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "['<Super>6']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-7 "['<Super>7']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-8 "['<Super>8']"
+
+gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab']"
+
+
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "[]"
+
+
+gsettings set org.gnome.desktop.wm.keybindings close "['<Super><Shift>q']"
+gsettings set org.gnome.desktop.wm.keybindings minimize "[]"
+gsettings set org.gnome.desktop.wm.keybindings toggle-fullscreen "['<Super>f']"
+gsettings set org.gnome.desktop.wm.keybindings toggle-maximized "['<Shift><Super>space']"
+gsettings set org.gnome.desktop.wm.keybindings toggle-on-all-workspaces "['<Shift><Super>s']"
+
+
+gsettings set org.gnome.shell.keybindings toggle-application-view "['<Super>d']"
+
+gsettings set org.gnome.shell.keybindings switch-to-application-1 "['<Super>F1']"
+gsettings set org.gnome.shell.keybindings switch-to-application-2 "['<Super>F2']"
+gsettings set org.gnome.shell.keybindings switch-to-application-3 "['<Super>F3']"
+gsettings set org.gnome.shell.keybindings switch-to-application-4 "['<Super>F4']"
+gsettings set org.gnome.shell.keybindings switch-to-application-5 "['<Super>F5']"
+gsettings set org.gnome.shell.keybindings switch-to-application-6 "['<Super>F6']"
+gsettings set org.gnome.shell.keybindings switch-to-application-7 "['<Super>F7']"
+gsettings set org.gnome.shell.keybindings switch-to-application-8 "['<Super>F8']"
+gsettings set org.gnome.shell.keybindings switch-to-application-9 "['<Super>F9']"
+
+
+gsettings set org.gnome.shell.window-switcher app-icon-mode 'app-icon-only'
+
+
+gsettings set org.gnome.settings-daemon.plugins.media-keys logout '<Super>0'
