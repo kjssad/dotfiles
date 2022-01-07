b85feb9a10b6bf8f14d7f531cc4c125ed7875eaa
kjssad <kimsadomia@protonmail.com>
2022-01-07 09:28:39 +0800

    feat(apt): update package install script

--- install/apt.sh
+++ install/apt.sh
@@ -2,4 +2,25 @@
 
 echo -e "\\nInstalling apt packages."
 echo "=============================="
-apt install curl gawk git highlight kitty neovim ripgrep shellcheck tmux zsh
+apt install \
+	bat \
+	build-essential \
+	curl \
+	fd-find \
+	fonts-roboto \
+	fonts-jetbrains-mono \
+	gawk \
+	git \
+	htop \
+	kitty \
+	lm-sensors \
+	ripgrep \
+	shellcheck \
+	tmux \
+	wl-clipboard \
+	zsh
+
+
+echo -e "\\nInstalling latex packages."
+echo "=============================="
+apt install texlive-xetex latexmk
