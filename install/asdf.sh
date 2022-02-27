534c8946d7384db54b92ab8bae092387bd98f756
kjssad <kimsadomia@protonmail.com>
2022-02-27 08:25:55 +0800

    refactor(asdf): update install script

--- install/asdf.sh
+++ install/asdf.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf
-cd ~/.asdf
-git checkout "$(git describe --abbrev=0 --tags)"
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.9.0
