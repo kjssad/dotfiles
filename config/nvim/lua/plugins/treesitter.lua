42f5d847402f80e2049bcc48bd288c217982f514
kjssad <kimsadomia@protonmail.com>
2022-03-18 08:05:04 +0800

    feat(vim): add treesitter select and move textobjects

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -39,6 +39,33 @@ local config = {
         ["if"] = "@function.inner",
         ["ac"] = "@class.outer",
         ["ic"] = "@class.inner",
+        ["ak"] = "@block.outer",
+        ["ik"] = "@block.inner",
+        ["ir"] = "@parameter.inner",
+      },
+    },
+    move = {
+      enable = true,
+      set_jumps = true,
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+        ["]]"] = "@class.outer",
+        ["]k"] = "@block.outer",
+      },
+      goto_next_end = {
+        ["]M"] = "@function.outer",
+        ["]["] = "@class.outer",
+        ["]K"] = "@block.outer",
+      },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+        ["[["] = "@class.outer",
+        ["[k"] = "@block.outer",
+      },
+      goto_previous_end = {
+        ["[M"] = "@function.outer",
+        ["[]"] = "@class.outer",
+        ["[K"] = "@block.outer",
       },
     },
   },
